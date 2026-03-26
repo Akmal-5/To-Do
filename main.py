c05d5f21aa1861e596a1373e1374aafb8f78276d
Akmal <amirokhanov5@gmail.com>
2026-03-26 19:24:10 +0500

    update main.py add some routs

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI , Depends , status , HTTPException
 from db.create_db import create_tables
+from models.models_data import User
+from db.config import AsyncSessionMaker
+from sqlalchemy.ext.asyncio import AsyncSession
+from typing import Annotated
+from db.data_manipulation import (create_user)
+
+async def get_session () :
+    async with AsyncSessionMaker() as session :
+        yield session
 
 app = FastAPI(
     title="To_Do 📝"
@@ -16,3 +25,21 @@ async def home () :
     return {
         "message" : "Главная страница ⬆"
     } 
+    
+@app.post("/auth/register/" ,
+          summary="Регистрация ✅",
+          tags=["Users"] ,
+          status_code=status.HTTP_201_CREATED)
+
+async def user_register (user : User , 
+                         session : Annotated[AsyncSession , Depends(get_session)] ,
+                         ) :
+    result = await create_user(session , user)
+    
+    if result :
+        await session.commit()
+        
+        return result
+    
+    raise HTTPException(status_code=status.HTTP_409_CONFLICT,
+                        detail= "This user already exists in the database.") 
