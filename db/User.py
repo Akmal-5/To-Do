e3d5983d423257f2d8bcbe5244f2f8b216175457
Akmal <amirokhanov5@gmail.com>
2026-03-26 17:08:05 +0500

    update

--- db/User.py
+++ db/User.py
@@ -1,5 +1,5 @@
 from db.config import Base
-from sqlalchemy import String
+from sqlalchemy import String , ForeignKey
 from sqlalchemy.orm import Mapped ,  mapped_column
 
 class User (Base) :
@@ -9,3 +9,13 @@ class User (Base) :
     username : Mapped[str] = mapped_column(String(70))
     password : Mapped[str] = mapped_column(String(100))
     email  : Mapped[str] = mapped_column(String(200))
+    
+
+class UsersNote(Base) :
+    
+    __tablename__ = "users_note"
+    id : Mapped[int] = mapped_column(primary_key=True)
+    user_id : Mapped[int] = mapped_column(ForeignKey("users.id"))
+    title : Mapped[str] = mapped_column(String(200) ,
+                                        nullable=False)    
+    description : Mapped[str] = mapped_column(String(300))
--- main.py
+++ main.py
@@ -1,9 +1,18 @@
-from fastapi import FastAPI 
+from fastapi import FastAPI
+from db.create_db import create_tables
 
-app = FastAPI(title="ToDo-project📝")
+app = FastAPI(
+    title="To_Do 📝"
+)
 
-@app.get("/home/" , summary="Главная страница1️⃣")
+@app.on_event("startup")
+async def on_startup():
+    await create_tables()
+
+@app.get("/home/" ,
+         summary="Главная страница" ,
+         tags=["Home⬆"])
 async def home () :
     return {
-        "message" : "Главная страница"
-    }
+        "message" : "Главная страница ⬆"
+    } 
