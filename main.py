ddd2f234adf9620a9c47a66e032bb23f82f97a88
Akmal <amirokhanov5@gmail.com>
2026-03-31 21:30:09 +0500

    update main.py

--- main.py
+++ main.py
@@ -57,5 +57,6 @@ async def user_login (userlog : UserLog ,
         return {
             "token" : res  
         }
+        
     raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
                         detail="Incorrect data or you are not registered")
