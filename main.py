e8685f0cdf985a3f4590f46a3a51ab6aedd425f2
Akmal <amirokhanov5@gmail.com>
2026-04-02 18:18:30 +0500

    update main.py add rout delete_task

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI , Depends , status , HTTPException , Query
+from fastapi import FastAPI , Depends , status , HTTPException , Query , Path
 from db.create_db import create_tables
 from models.models_data import User , UserLog , UserTasks
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -6,7 +6,8 @@ from typing import Annotated
 from db.data_manipulation import (create_user , 
                                   user_verification,
                                   create_user_task,
-                                  get_users_task
+                                  get_users_task,
+                                  delete_tasks
                                   )
 from Depends.create_session import get_session
 from Depends.auth_scheme import encode_jwt , decode_jwt
@@ -47,7 +48,7 @@ async def user_register (user : User ,
     raise HTTPException(status_code=status.HTTP_409_CONFLICT,
                         detail= "This user already exists in the database.")
 
-@app.post("/auth/login" , summary="Авторизация🔐" , tags=["Users🙍‍♂️"])
+@app.post("/auth/login/" , summary="Авторизация🔐" , tags=["Users🙍‍♂️"])
 async def user_login (userlog : UserLog ,
                       session : Annotated[AsyncSession , Depends(get_session)]) :
     
@@ -85,3 +86,16 @@ async def get_task (session : Annotated[AsyncSession , Depends(get_session)],
                     filtering_by_title : Annotated[str | None, Query()] = None) :
     
     return await get_users_task(session , user_id , filtering_by_title)
+
+@app.delete("/tasks/delete_task/{task_id}/",
+            summary="Удолить задачу",
+            tags=["Операции над задачами 🔍"]
+            )
+async def  delete_task (session : Annotated[AsyncSession , Depends(get_session)] ,
+                        user_id : Annotated[int , Depends(decode_jwt)],
+                        task_id : Annotated[int ,Path()]
+                        ) :
+    result = await  delete_tasks(session ,  user_id , task_id)
+    if result :
+        return result
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND) 
